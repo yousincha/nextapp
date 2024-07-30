af9e5c4f65cd82ea32479a1d88b162453c11c589
[yousincha] <[yousincha@naver.com]>
2024-07-30 16:53:55 +0900

    20240730-3 (GET,POST,READ 추가) commit

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,3 +1,44 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Create() {
-  return <>Create!!</>;
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // 이 부분을 수정했습니다.
+
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, body }),
+    };
+
+    try {
+      const response = await fetch(`http://localhost:9999/topics`, options);
+      const result = await response.json();
+      console.log(result);
+      const lastid = result.id;
+      router.push(`/read/${lastid}`);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
 }
