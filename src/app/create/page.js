1163580c2ce70ab5b4d6ac728588545a3685e722
[yousincha] <[yousincha@naver.com]>
2024-07-31 17:20:29 +0900

    20240731-5 (환경변수 추가) commit

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -16,7 +16,7 @@ export default function Create() {
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ title, body }),
         };
-        fetch(`http://localhost:9999/topics`, options)
+        fetch(process.env.NEXT_PUBLIC_API_URL + `topics`, options)
           .then((res) => res.json())
           .then((result) => {
             console.log(result);
